b60b838fb9aacba86dbd81a8a902125eec287143
Thomas Parrish <thomas.parrish@Toms-Mac.local>
2018-09-30 23:21:21 -0700

    Added events from Wwise

--- _macOS/WwiseSFX.js
+++ _macOS/WwiseSFX.js
@@ -16,7 +16,7 @@ connection.onopen = function (session) {
  from: { path: ['\\Events\\Default Work Unit\\SFX\\'] },
  transform: [
 	 {select:['descendants']},
-	 {type:['Event']}
+	 {typeof:['Event']}
  ]
  };
  
