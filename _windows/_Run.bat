8b4d84d9e254c413f00e225a97ec30222991aa5b
Thomas Parrish <thomasrparrish@gmail.com>
2018-10-08 18:15:51 -0700

    Added events from Wwise

--- _windows/_Run.bat
+++ _windows/_Run.bat
@@ -1,6 +1,7 @@
-node WwiseSFX.js > ../WwiseSFX.txt
-node WwiseMUS.js > ../WwiseMUS.txt
-node WwiseVO.js > ../WwiseVO.txt
+node ../waapi/WwiseSFX.js > ../WwiseSFX.txt
+node ../waapi/WwiseMUS.js > ../WwiseMUS.txt
+node ../waapi/WwiseVO.js > ../WwiseVO.txt
 git add --all
 git commit -m "Added events from Wwise"
+git pull
 git push
