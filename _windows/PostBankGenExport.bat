b5ee89b94cc3ff58925e4258a029c842ea71f086
Thomas Parrish <thomasrparrish@gmail.com>
2018-10-08 21:21:37 -0700

    Added events from Wwise

--- _windows/PostBankGenExport.bat
+++ _windows/PostBankGenExport.bat
@@ -1,8 +1,13 @@
-SET projPath=C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport
-node %projPath%\waapi\WwiseSFX.js > %projPath%\WwiseSFX.txt
-node %projPath%\waapi\WwiseMUS.js > %projPath%\WwiseMUS.txt
-node %projPath%\waapi\WwiseVO.js > %projPath%\WwiseVO.txt
-git -C %projPath%\ add --all
-git -C %projPath%\ commit -m "Added events from Wwise"
-git -C %projPath%\ pull
-git -C %projPath%\ push
+::Path of 'SheetsWwiseImport' repository clone. Recommended to place in Wwise project root folder.
+SET SheetExportDir=C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport
+
+::Reroute output from Node.js scripts into the corresponding text files.
+node %SheetExportDir%\waapi\WwiseSFX.js > %SheetExportDir%\WwiseSFX.txt
+node %SheetExportDir%\waapi\WwiseMUS.js > %SheetExportDir%\WwiseMUS.txt
+node %SheetExportDir%\waapi\WwiseVO.js > %SheetExportDir%\WwiseVO.txt
+
+::Commit all changes, then upload to GitHub repository.
+git -C %SheetExportDir%\ add --all
+git -C %SheetExportDir%\ commit -m "Added events from Wwise"
+git -C %SheetExportDir%\ pull
+git -C %SheetExportDir%\ push
