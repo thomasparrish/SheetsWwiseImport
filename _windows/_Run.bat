4326dbe9c5387847d267aafd739d74618f44a7c4
Thomas Parrish <thomasrparrish@gmail.com>
2018-10-08 19:17:19 -0700

    Added events from Wwise

--- _windows/_Run.bat
+++ _windows/_Run.bat
@@ -1,6 +1,6 @@
-node ../waapi/WwiseSFX.js > ../WwiseSFX.txt
-node ../waapi/WwiseMUS.js > ../WwiseMUS.txt
-node ../waapi/WwiseVO.js > ../WwiseVO.txt
+node C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport\waapi\WwiseSFX.js > C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport\WwiseSFX.txt
+node C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport\waapi\WwiseMUS.js > C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport\WwiseMUS.txt
+node C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport\waapi\WwiseVO.js > C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport\WwiseVO.txt
 git add --all
 git commit -m "Added events from Wwise"
 git pull
