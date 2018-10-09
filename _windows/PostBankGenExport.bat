d65b0e1018443fc84e7fe635367e68723c323801
Thomas Parrish <thomasrparrish@gmail.com>
2018-10-09 15:58:20 -0700

    Added events from Wwise

--- _windows/PostBankGenExport.bat
+++ _windows/PostBankGenExport.bat
@@ -1,5 +1,5 @@
 ::Path of 'SheetsWwiseImport' repository clone. Recommended to place in Wwise project root folder.
-SET SheetExportDir=C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport
+SET SheetExportDir=C:\Users\thomas.parrish\Documents\WwiseProjects\WwiseTest\SheetsWwiseImport
 
 ::Reroute output from Node.js scripts into the corresponding text files.
 node %SheetExportDir%\waapi\WwiseSFX.js > %SheetExportDir%\WwiseSFX.txt
