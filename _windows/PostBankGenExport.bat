c21d65d24a63ac8ea6007a26e88b029cf6d82cf2
Thomas Parrish <thomasrparrish@gmail.com>
2018-10-08 21:05:18 -0700

    Added events from Wwise

--- _windows/PostBankGenExport.bat
+++ _windows/PostBankGenExport.bat
@@ -2,8 +2,8 @@ SET projPath=C:\Users\Tommy\Documents\WwiseProjects\First2018\SheetsWwiseImport
 node %projPath%\waapi\WwiseSFX.js > %projPath%\WwiseSFX.txt
 node %projPath%\waapi\WwiseMUS.js > %projPath%\WwiseMUS.txt
 node %projPath%\waapi\WwiseVO.js > %projPath%\WwiseVO.txt
-git add --all %projPath%\
-git commit %projPath%\ -m "Added events from Wwise"
-git pull %projPath%\
-git push %projPath%\
+git -C %projPath%\ add --all
+git -C %projPath%\ commit -m "Added events from Wwise"
+git -C %projPath%\ pull
+git -C %projPath%\ push
 pause
